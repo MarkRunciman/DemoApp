b22603bdf9d8c1d77caf4bd35d2fedaeb8efdf7a
MarkRunciman <58737447+MarkRunciman@users.noreply.github.com>
2025-06-25 12:29:24 +0100

    Delete api directory

--- api/products/index.js
+++
@@ -1,7 +0,0 @@
-const getPool = require('../shared/db');
-
-module.exports = async function (context, req) {
-    const pool = await getPool();
-    const result = await pool.request().query('SELECT * FROM Products');
-    context.res = { status: 200, body: result.recordset };
-};
